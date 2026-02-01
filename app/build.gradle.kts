9eeeb5898e5079884bb2cece06596e7926b090d2
kalzEOS <49320606+kalzEOS@users.noreply.github.com>
2026-02-01 01:24:05 -0500

    Testing size of update window

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.example.xtreamplayer"
         minSdk = 24
         targetSdk = 36
-        versionCode = 51
-        versionName = "2.2.11"
+        versionCode = 52
+        versionName = "2.2.12"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
@@ -85,7 +85,7 @@ androidComponents {
             val outputFileName = runCatching {
                 output::class.java.getMethod("getOutputFileName").invoke(output) as Property<String>
             }.getOrNull()
-            outputFileName?.set("Xtream Playerv2.2.11.apk")
+            outputFileName?.set("Xtream Playerv2.2.12.apk")
         }
     }
 }
