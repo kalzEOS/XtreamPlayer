706df8d434b111dd20b88a2a32f82d21cc0fd2f6
kalzEOS <49320606+kalzEOS@users.noreply.github.com>
2026-04-11 22:53:03 -0400

    Restore dimmed player controls

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,8 +3,8 @@ import java.util.Properties
 import org.gradle.api.provider.Property
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
-val appVersionCode = 138
-val appVersionName = "3.4.2"
+val appVersionCode = 139
+val appVersionName = "3.4.3"
 
 plugins {
     alias(libs.plugins.android.application)
