cbb453d3a2f6858c3df481388cd42206bcca3058
kalzEOS <49320606+kalzEOS@users.noreply.github.com>
2026-05-17 15:38:35 -0400

    Bump release version to 3.4.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,8 +3,8 @@ import java.util.Properties
 import org.gradle.api.provider.Property
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
-val appVersionCode = 142
-val appVersionName = "3.4.6"
+val appVersionCode = 143
+val appVersionName = "3.4.7"
 
 plugins {
     alias(libs.plugins.android.application)
